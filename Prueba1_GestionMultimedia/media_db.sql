3b6e18f0fc96ca6cc9f4f455155ec8f2bc839190
Juan Esteban Valverde Mora <valverdemorajuanesteban@gmail.com>
2025-03-19 11:07:18 -0600

    Cambios en la bd

--- Prueba1_GestionMultimedia/media_db.sql
+++ Prueba1_GestionMultimedia/media_db.sql
@@ -7,3 +7,12 @@ create table multimedia(
     url varchar(500),
     tipo varchar(50)
 );
+
+INSERT INTO multimedia(titulo,descripcion,url,tipo) 
+	VALUES ("Aprendiendo CSS", "Curso desde 0","https://www.youtube.com/watch?v=hrxjBqZWsb0&ab_channel=midulive","Video");
+INSERT INTO multimedia(titulo,descripcion,url,tipo) 
+	VALUES ("Montana", "Fondo para escritorio",
+    "https://image.slidesdocs.com/responsive-images/background/monochrome-dreamland-gray-featuring-mountains-trees-clouds-and-the-moon-in-3d-canvas-powerpoint-background_8c4545aa7f__960_540.jpg",
+    "Imagen");
+INSERT INTO multimedia(titulo,descripcion,url,tipo) 
+	VALUES ("Grateful", "Cancion de NEFFEX","https://music.youtube.com/watch?v=dAgNwavNAHQ&list=RDAMVMdAgNwavNAHQ","Audio");
